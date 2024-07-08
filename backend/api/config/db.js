5e4169a9f5b7cc5c2ff3f933f2ad547912abe9b2
165117-tmkhoa <truongminhkhoa1999vn@gmail.com>
2024-07-08 22:19:42 +0700

    Migrate node new version module library modify connection mongodb

--- backend/api/config/db.js
+++ backend/api/config/db.js
@@ -36,7 +36,7 @@ function initial() {
 const connectDB = async () => {
   const conn = await mongoose
     .connect(
-      process.env.DB_CONN_STRING || "mongodb://127.0.0.1:27017/book-shop",
+      process.env.DB_CONN_STRING || "mongodb+srv://nhoxtin456:Admin123@cluster0.9whoysl.mongodb.net/test/?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useCreateIndex: true,
--- backend/api/seeder.js
+++ backend/api/seeder.js
@@ -2,6 +2,8 @@ import fs from "fs";
 import mongoose from "mongoose";
 import colors from "colors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 
 //load env vars
@@ -17,7 +19,7 @@ import Bill from "./models/bill.model.js";
 // Connect to DB
 mongoose.connect(
   process.env.MONGO_DB_URI ||
-    "mongodb+srv://myMongoDBUser:Abc123456@cluster0.kpppm.mongodb.net/bookshop?retryWrites=true",
+  "mongodb+srv://nhoxtin456:Admin123@cluster0.9whoysl.mongodb.net/test/?retryWrites=true&w=majority&appName=Cluster0",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -26,18 +28,21 @@ mongoose.connect(
   }
 );
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Read JSON files
 const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/data/users.json`, "utf-8")
 );
 const products = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/product.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/data/product.json`, "utf-8")
 );
 const categories = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/category.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/data/category.json`, "utf-8")
 );
 const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/data/reviews.json`, "utf-8")
 );
 
 //Import into DB
